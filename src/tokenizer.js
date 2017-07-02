15da2ae69482456b713e5adfc9b5f94b20237334
molingyu <z1422716486@hotmail.com>
2017-07-02 14:12:47 +0800

    update tokenizer

--- src/tokenizer.js
+++ src/tokenizer.js
@@ -2,15 +2,36 @@ import error from './error'
 
 class Tokenizer {
 
-  constructor(pos, code, lineCode) {
+  constructor(pos, code) {
     this.code = code
     this.index = 0
     this.pos = pos
+    this.arithmetics = [
+      '!',
+      '~',
+      '(',
+      ')',
+      '*',
+      '/',
+      '%',
+      '+',
+      '-',
+      '>',
+      '<',
+      '>=',
+      '>=',
+      '==',
+      '!=',
+      '>>>',
+      '===',
+      '!==',
+      '='      
+    ]
   }
 
   run() {
     let tokens = []
-    while(this.chr() != void 0) {
+    while (this.chr() != void 0) {
       tokens.push(this.exp(), this.chr())
     }
     return tokens
@@ -21,19 +42,25 @@ class Tokenizer {
   }
 
   nextIs(chr) {
-    return this.code[this.index] == chr
+    return this.code[this.index + 1] == chr
   }
 
   ws() {
-
+    while (this.chr() == ' ') {
+      this.index += 1;
+    }
   }
 
   chr() {
     return this.code[this.index]
   }
 
-  skip() {
-
+  skip(str) {
+    for (let i in str) {
+      if (str[i] != this.chr()) return false
+      this.index += 1
+    }
+    return true
   }
 
   getPos() {
@@ -42,7 +69,7 @@ class Tokenizer {
     return pos
   }
 
-  exp() {
+  expression() {
     this.ws()
     let chr = this.chr()
     if (chr.match(/[A-Z]/)) {
@@ -77,13 +104,28 @@ class Tokenizer {
         value: this.string()
       }
     }
-    if (chr = '/') {
+    if(this.arithmetics.includes(this.chr)) {
       return {
-        type: 'string',
+        type: 'arithmetic',
         pos: this.index,
-        value: this.regexp()
+        value: this.arithmetic()
       }
     }
+    if(this.skip('typeof')){
+      return {
+        type: 'arithmetic',
+        pos: this.index,
+        value: 'typeof'
+      }
+    }
+    if(this.skip('void')){
+      return {
+        type: 'arithmetic',
+        pos: this.index,
+        value: 'void'
+      }
+    }
+    error(SyntaxError, 'Invalid or unexpected token', this.getPos())
   }
 
   number() {
@@ -91,7 +133,7 @@ class Tokenizer {
     let decHas = false
     let eHas = false
     while (true) {
-      if(this.chr() == void 0) break
+      if (this.chr() == void 0) break
       if (this.chr().match(/[0-9.]/)) {
         if (this.chr() == '.') {
           if (decHas || eHas) {
@@ -110,33 +152,84 @@ class Tokenizer {
             number += this.chr()
             this.next()
           }
+        } else if (this.chr().match(/[+-]/)) {
+          if (this.code[this.index - 1].match(/[eE]/)) {
+            number += this.chr()
+            this.next()
+          }
+        } else {
+          break
         }
-        // if() {
-
-        // }
-        break
       }
+    }
+    number = Number(number)
+    if(isNaN(number)) {
+      error(SyntaxError, 'Bad number', this.getPos())
+    } else {
+      return number
     }
   }
 
   string() {
-
-  }
-
-  regexp() {
-
+    let string = ''
+    let startStr = this.chr()
+    let lastIs = false
+    this.next()
+    while (true) {
+      if(this.chr() == void 0) error(SyntaxError, 'Invalid or unexpected token', this.getPos())
+      if (this.chr() == '/') {
+        lastIs = !lastIs
+        if (lastIs && this.nextIs(startStr)) {
+          this.next()
+          break
+        }
+        string += this.chr()
+        this.next()
+      }
+    }
   }
 
   identifier() {
-
+    let identifier = ''
+    while(this.chr().match(/[_a-z0-9]/)) {
+      identifier += this.chr()
+    }
+    return identifier
   }
 
   globalIdentifier() {
-
+    let globalIdentifier = '$'
+    this.next()
+    while(this.chr().match(/[_a-z0-9]/)) {
+      globalIdentifier += this.chr()
+    }
+    return globalIdentifier
   }
 
   flag() {
+    let flag = ''
+    while(this.chr().match(/[_A-Z0-9]/)) {
+      flag += this.chr()
+    }
+    return flag
+  }
 
+  arithmetic() {
+    let arithmetic = this.chr()
+    this.next()
+    if (this.nextIs('=')) {
+      arithmetic += this.chr()
+      this.next()
+      if (this.nextIs('=')) {
+        arithmetic += this.chr()
+        this.next()
+      }
+    } else {
+      if (this.skip('>>') && this.code[this.index - 1] == '>') {
+        arithmetic = '>>>'
+      }
+    }
+    return arithmetic
   }
 
 }
