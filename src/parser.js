a1a3c38893bfb641b1a4305e3e2e43bb308f210f
molingyu <z1522716486@hotmail.com>
2017-07-06 18:47:19 +0800

    update complier
    fix identifier function error. delete `global` of identifier node.

--- src/parser.js
+++ src/parser.js
@@ -5,6 +5,7 @@ class Parser {
   constructor() {
     this.nodeStack = []
     this.list = []
+    this.nodeIndex = 0
   }
 
   parse(codeStr, file = '') {
@@ -47,55 +48,150 @@ class Parser {
         script += line + '\n';
       }
     })
+    if (this.node != ASTRoot) {
+      let pos = this.getPos(start, 0)
+      pos.cols = codes[codes.length - 1].length - 1
+      error(SyntaxError, 'Unexpected endif of input', pos)
+  }
     return ASTRoot;
   }
 
-  reList() {
-    this.node = this.nodeStack[this.nodeStack.length - 1]
-    if (this.node.type == 'ifStatement') {
-      this.list = this.node.isConsequent ? this.node.ifConsequent : this.node.ifAlternate
-    } else {
-      this.list = this.node.child
+reList() {
+  this.node = this.nodeStack[this.nodeStack.length - 1]
+  if (this.node.type == 'ifStatement') {
+    this.list = this.node.isConsequent ? this.node.ifConsequent : this.node.ifAlternate
+  } else {
+    this.list = this.node.child
+  }
+}
+
+getPos(posObject, index) {
+  let pos = Object.assign({}, posObject)
+  pos.cols += index
+  return pos
+}
+
+code(codeStr, start) {
+  this.tokens = new Tokenizer(codeStr, start).run()
+  while (this.token() != void 0) {
+    switch (token.type) {
+      case 'value':
+        return {
+          type: 'value',
+          value: token.value,
+          pos: this.getPos(start, value.pos)
+        }
+      case 'keyword':
+        return this.keyword(start)
+      case 'flag':
+        return {
+          type: 'flag',
+          identifier: this.token().value,
+          pos: this.getPos(start, this.token().pos)
+        }
+      case 'globalIdentifier':
+        return this.identifier(start)
+      case 'identifier':
+        return this.identifier(start)
+      default:
+        error(SyntaxError, 'error token type', this.getPos(start, this.nodeIndex))
     }
   }
+}
 
-  getPos(posObject, index) {
-    let pos = Object.assign({}, posObject)
-    pos.cols += index
-    return pos
+token() {
+  return this.tokens[this.nodeIndex]
+}
+
+next() {
+  this.nodeIndex += 1
+}
+
+isOnly(pos) {
+  this.next()
+  if (this.token() != void 0) {
+    error(SyntaxError, `Unexpected token ${this.token().value}`, this.getPos(pos, this.token().pos))
   }
+}
 
-  code(codeStr, start) {
-    let tokens = new Tokenizer(codeStr, start).run()
-    let nodeStack = []
-    let node
-    tokens.forEach((token) => {
-      switch (token.type) {
-        case 'value':
-          node = {
-            type: 'value',
-            value: token.value,
-            pos: this.getPos(start, value.pos)
-          }
-          break
-        case 'keyword':
+identifier(start) {
+  if (this.tokens[this.tokens.length - 1] == this.token()) {
+    return {
+      type: 'identifier',
+      identifier: this.token().value,
+      pos: this.getPos(start, this.token().pos)
+    }
+  } else {
+    let node = {}
+    node.type = 'variableUpdate'
+    node.identifier = this.token().value
+    node.pos = this.getPos(start, this.token().pos)
+    this.next()
+    if (this.token().value == '=') {
+      node.value = this.expression(start)
+    } else {
+      error(SyntaxError, `Unexpected token ${this.token().value}`, this.getPos(start, posIndex))
+    }
+  }
+}
 
-          break
-        case 'flag':
+expression(start) {
+  
+}
 
-          break
-        case 'globalIdentifier':
-
-          break
-        case 'identifier':
-
-          break
-        case 'arithmetic':
-
-          break
+keyword(start) {
+  let node = {}
+  let posIndex = this.token().pos
+  switch (this.token().value) {
+    case 'define':
+      this.next()
+      let obj = {
+        'globalIdentifier': 'globalVariableDeclaration',
+        'identifier': 'variableDeclaration'
       }
-    })
+      if (obj[this.token().type] != void 0) {
+        node.type = obj[this.token().type]
+        node.identifier = this.token().value
+        this.next()
+        if (this.token().value == '=') {
+          this.next()
+          node.value = this.expression(start)
+          node.pos = this.getPos(start, posIndex)
+        } else {
+          error(SyntaxError, `Unexpected token ${this.token().value}`, this.getPos(start, posIndex))
+        }
+      } else {
+        error(SyntaxError, `Unexpected token ${this.token().value}`, this.getPos(start, posIndex))
+      }
+      break
+    case 'if':
+      node.type = 'ifStatement'
+      node.test = this.expression(start)
+      node.ifConsequent = []
+      node.ifAlternate = []
+      node.isConsequent = true
+      node.pos = this.getPos(start, posIndex)
+      this.nodeStack.push(node)
+      break
+    case 'else':
+      this.isOnly(start)
+      if (this.node.type != 'ifStatement') {
+        error(SyntaxError, 'Unexpected token else', this.getPos(start, posIndex))
+      }
+      this.node.isConsequent = false
+      break
+    case 'endif':
+      this.isOnly(start)
+      if (this.node.type != 'ifStatement') {
+        error(SyntaxError, 'Unexpected token endif', this.getPos(start, posIndex))
+      }
+      delete this.node.isConsequent
+      this.nodeStack.pop()
+      break
+    default:
+      error()
   }
+}
 
 }
 
